cc3eebb9f4d56827decd9e93c5a1a6c304dcbc7d
june0216 <ziyun1612@ewhain.net>
2025-01-16 17:22:55 +0900

    fix : 라우터 변경 시 새로고침되어 context가 유지되지 않는 문제 해결

--- src/components/common/MoveButton.jsx
+++ src/components/common/MoveButton.jsx
@@ -1,16 +1,20 @@
 import PropTypes from "prop-types";
 
-export const MoveButton = ({content, moveUrl}) => {
+import { useNavigate } from "react-router-dom";
+
+export const MoveButton = ({ content, moveUrl }) => {
+    const navigate = useNavigate();
+
     return (
         <button
             className="hover:bg-[#506CFF] hover:text-white rounded-[8px] border border-[#506CFF] text-[#506CFF] text-[14px] weight-600 px-[21px] py-[5px]"
-            onClick={() => {
-                location.href = "http://localhost:5173/" + moveUrl;
-            }}>
+            onClick={() => navigate(moveUrl)} // React Router의 useNavigate로 이동
+        >
             {content}
         </button>
     );
-}
+};
+
 
 MoveButton.propTypes = {
     content: PropTypes.string.isRequired,
