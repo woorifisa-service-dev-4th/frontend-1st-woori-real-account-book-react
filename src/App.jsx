b12dff4e45be8513d784d3745226ec2cc356da62
mjgwon24 <alswlchlrh8@naver.com>
2025-01-16 09:57:43 +0900

    feat: App.jsx 공통 Provider 적용 및 MainHeader파일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import {AccountBookProvider} from "./contexts/AccountBookContext.jsx";
+import MainHeader from "./components/MainHeader.jsx";
 
 function App() {
 
   return (
     <>
+      <AccountBookProvider>
+        <MainHeader />
+      </AccountBookProvider>
     </>
   )
 }
