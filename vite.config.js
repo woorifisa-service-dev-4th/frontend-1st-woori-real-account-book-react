3335ef7e311237e80516cf51de933a9b02fb5e00
Jiyun Kwon <ziyun1612@ewhain.net>
2025-01-17 01:26:06 +0900

    fix : Rollup is unable 문제 해결

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-
-// https://vite.dev/config/
+import resolve from '@rollup/plugin-node-resolve';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
