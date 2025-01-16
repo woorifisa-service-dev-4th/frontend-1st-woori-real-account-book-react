6cdd34dcd6d43083f21a0148800d5bf696ccc9c4
june0216 <ziyun1612@ewhain.net>
2025-01-16 23:11:08 +0900

    feat : GitHub Pages에 배포할 때 base 경로를 추가

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
       { find: '@', replacement: '/src' }
     ]
-  }
-})
+  },
+  base: '/frontend-1st-woori-real-account-book-react/', // GitHub Pages에서 사용할 기본 경로
+});
